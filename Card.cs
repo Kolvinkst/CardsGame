97a4ab14f97e9e7909c8095dfa22819d1444ba77
Kolvinkst <einbenutzer1998@gmail.com>
2023-12-17 08:26:32 +0700

    Добавьте файлы проекта.

---
+++ Card.cs
@@ -0,0 +1,23 @@
+﻿using CardsGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsGame
+{
+    internal class Card
+    {
+        public Values Value { get; private set; }
+        public Suits Suit { get; private set; }
+
+public Card(Values value, Suits suit)
+{
+    this.Suit = suit;
+    this.Value = value;
+}
+public string Name { get { return $"{Value} of {Suit}"; } }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsGame
+{
+    internal class Program
+    {
+        private static readonly Random random = new Random();
+        static void Main(string[] args)
+        {
+            Card card = new Card((Values)random.Next(1, 14), (Suits)random.Next(4));
+            Console.WriteLine(card.Name);
+            Console.WriteLine("Press any key to exit");
+            char key = Console.ReadKey().KeyChar;
+        }
+    }
+}
